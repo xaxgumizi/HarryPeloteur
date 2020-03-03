af6cb82114148f96d089e9c85d55dc15c2bb2562
xaxgumizi <xaxgumizi>
2020-03-03 15:41:15 +0100

    Mise à jour de insertroom

--- HarryPeloteur_DAL/DBController.cs
+++ HarryPeloteur_DAL/DBController.cs
@@ -29,8 +29,8 @@ namespace HarryPeloteur_DAL
         }
         public int InsertRoom(SalleDTO salle)
         {
-            string coordText = ArrayToString(salle.coordonnees);
-            string portesText = ArrayToString(salle.portes);
+            string coordText = ArrayToString(salle.Coordonnees);
+            string portesText = ArrayToString(salle.Portes);
 
             string q = "insert into salle(coordonnees,id_contenu,type_contenu,portes,etat,id_partie) values('" + coordText + "'," + salle.id_contenu + "," + salle.type_contenu + ",'" + portesText + "'," + salle.etat + "," + salle.id_partie + ") SELECT SCOPE_IDENTITY()";
             SqlCommand cmd = new SqlCommand(q, this.con);
